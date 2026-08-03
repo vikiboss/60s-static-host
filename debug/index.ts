2ffd16847b64437b0dcff3df71d0b74cc40dd584
Viki <hi@viki.moe>
2026-08-03 08:28:13 +0800

    chore: save

--- debug/index.ts
+++ debug/index.ts
@@ -1,9 +1,24 @@
+import { formatSavedData, localeDate, localeTime } from '../src/utils'
 import { parsePostViaLLM } from '../src/services/parser'
+import type { SavedData } from '../src/services/storage'
 
-const url =
-  'https://mp.weixin.qq.com/s/1ZhaR1E_77o6aADmO2dUyA'
+const url = 'https://mp.weixin.qq.com/s/oE01Nq9iVFWj_i-pDIeKhQ'
 
-await parsePostViaLLM(url)
+const parsed = await parsePostViaLLM(url)
+
+const data: SavedData = {
+  date: localeDate(),
+  ...parsed,
+  image: `https://cdn.jsdmirror.com/gh/vikiboss/60s-static-host@main/static/images/${localeDate()}.png`,
+  cover: parsed.cover,
+  link: url,
+  created: localeTime(new Date()),
+  created_at: new Date().getTime(),
+  updated: localeTime(new Date()),
+  updated_at: new Date().getTime(),
+}
+
+console.log(formatSavedData(data))
 
 // import { wechat } from '../src/services/wechat'
 
