c19b3be18db00235c3b76c4a6906f4e4db51b761
Viki <hi@viki.moe>
2026-08-03 08:38:18 +0800

    chore: save

--- src/update-60s.tsx
+++ src/update-60s.tsx
@@ -20,10 +20,102 @@ update60s().catch(error => {
 })
 
 export async function update60s(): Promise<void> {
+  const url = getInputArgValue('url')
   const inputDate = getInputArgValue('date')
 
+  debug('inputURL', url || '[EMPTY URL]')
   debug('inputDate', inputDate || '[EMPTY DATE]')
 
+  // 2026.8.3 更新：
+  // 微信公众平台无法获取文章列表，需要手动传入最新的文章 url
+  // 当前已切换为使用 url 模式更新，通过微信公众平台获取途径暂不可用
+  if (url) {
+    console.log('当前检测到 URL，将直接通过文章 URL 获取数据')
+    await updateWithDataUrl(url)
+  } else {
+    console.log('当前未检测到 URL，将通过微信公众平台获取文章 URL 后再获取数据')
+    await updateWithDate(inputDate)
+  }
+
+  process.exit(0)
+}
+
+async function saveImage(data: SavedData): Promise<void> {
+  await renderer.prepare()
+  const buffer = await renderer.render(<NewsCard data={formatSavedData(data)} />)
+  await renderer.destroy()
+  await storage.saveImage(data.date, buffer)
+}
+
+async function updateWithDataUrl(url: string) {
+  const date = localeDate()
+
+  if (storage.hasData(date) && storage.hasImage(date)) {
+    log(`Data & Image of [${date}] already exists, skipped`)
+    process.exit(0)
+  }
+
+  if (storage.hasData(date)) {
+    const data = await storage.loadData(date)
+
+    if (!data) {
+      console.warn(`No data found for date: ${date}`)
+      process.exit(1)
+    }
+
+    log(`Data of [${date}] found, generating image...`)
+
+    await saveImage(data)
+
+    process.exit(0)
+  }
+
+  const parsed = await parsePostViaLLM(url).catch(async error => {
+    console.warn(`LLM parser failed, retrying... error:`, error)
+
+    try {
+      return await parsePostViaLLM(url)
+    } catch (error) {
+      console.warn(`LLM parser retry failed. error:`, error)
+      return null
+    }
+  })
+
+  if (!parsed) {
+    console.warn(`Parse failed, link: ${url}`)
+    process.exit(1)
+  }
+
+  if (!parsed.news.length) {
+    console.warn(`No news found for link: ${url}`)
+    process.exit(1)
+  }
+
+  debug('parsed', parsed)
+
+  const now = new Date()
+
+  const data: SavedData = {
+    date: date,
+    ...parsed,
+    image: `https://cdn.jsdmirror.com/gh/vikiboss/60s-static-host@main/static/images/${date}.png`,
+    cover: parsed.cover,
+    link: url,
+    created: localeTime(now),
+    created_at: now.getTime(),
+    updated: localeTime(now),
+    updated_at: now.getTime(),
+  }
+
+  debug('data', data)
+
+  await storage.saveData(data)
+  await saveImage(data)
+
+  log('Update 60s completed')
+}
+
+async function updateWithDate(inputDate?: string) {
   if (inputDate && !isValidDateFormat(inputDate)) {
     console.error('Invalid date format, expect: YYYY-MM-DD')
     process.exit(1)
@@ -150,11 +242,4 @@ export async function update60s(): Promise<void> {
 
     break
   }
-}
-
-async function saveImage(data: SavedData): Promise<void> {
-  await renderer.prepare()
-  const buffer = await renderer.render(<NewsCard data={formatSavedData(data)} />)
-  await renderer.destroy()
-  await storage.saveImage(data.date, buffer)
 }
